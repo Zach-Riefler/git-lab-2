07a0ab2ec7e240d94fbca9c21783f01cd736f9f9
Zach Riefler <zr420317@ohio.edu>
2023-09-08 18:06:23 -0400

    committing the lab

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+*       File: git-lab-program.cc
+*     Author: Zach Riefler
+*       Date: 9/8/2023
+*Description: #FIXME
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    
+    cout << "Hello Git!!" << endl;
+
+    return 0;
+
+} // main
